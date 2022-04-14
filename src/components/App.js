6af93e5f1d26a4c776dbfa07065dad92f44bfb81
kikako-saravanan <msaravanan1998@gmail.com>
2022-04-15 01:05:30 +0530

    main

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,162 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import "./styles.css";
+import { signUpFormValidation } from "./validation";
+import { useState } from "react";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [gender, setGender] = useState("male");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [user, setUser] = useState("");
+
+  const [errors, setErrors] = useState("");
+  const obj = {
+    name: name,
+    email: email,
+    password: password,
+    gender: gender,
+    phoneNumber: phoneNumber,
+  };
+
+  const handleClick = (ev) => {
+    ev.preventDefault();
+    const eve = signUpFormValidation(obj);
+    setErrors(eve);
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    if (e.target.id === "name") {
+      setName(e.target.value);
+      obj.name = e.target.value;
+      const eve = signUpFormValidation(obj);
+      setErrors(eve);
+    } else if (e.target.id === "email") {
+      setEmail(e.target.value);
+      obj.email = e.target.value;
+      const eve = signUpFormValidation(obj);
+      setErrors(eve);
+    } else if (e.target.id === "password") {
+      setPassword(e.target.value);
+      obj.password = e.target.value;
+      const eve = signUpFormValidation(obj);
+      setErrors(eve);
+    } else if (e.target.id === "gender") {
+      console.log(e.target.value);
+      setGender(e.target.value);
+      obj.gender = e.target.value;
+      const eve = signUpFormValidation(obj);
+      setErrors(eve);
+    } else if (e.target.id === "phoneNumber") {
+      setPhoneNumber(e.target.value);
+      obj.phoneNumber = e.target.value;
+      const eve = signUpFormValidation(obj);
+      setErrors(eve);
+    }
+    console.log(errors);
+    if (errors === null) {
+      const inde = obj.email.indexOf("@");
+      const userName = obj.email.slice(0, inde);
+      setUser(userName);
+    } else {
+      setUser("");
+    }
+  };
   return (
-    <div id="main"></div>
-  )
-}
+    <>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          data-testid="name"
+          onChange={handleChange}
+        />
+        <br />
+        {errors !== null && errors.name !== "" ? (
+          <span>{errors.name}</span>
+        ) : (
+          ""
+        )}
+        <br />
+        <br />
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          data-testid="email"
+          onChange={handleChange}
+        />
+        <br />
+        {errors !== null && errors.email !== "" ? (
+          <span>{errors.email}</span>
+        ) : (
+          ""
+        )}
+        <br />
+        <br />
+        <label htmlFor="gender">Gender: </label>
+        <select
+          data-testid="gender"
+          value={gender}
+          onChange={handleChange}
+          id="gender"
+        >
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">other</option>
+        </select>
+        <br />
+        {errors !== null && errors.gender !== "" ? (
+          <span>{errors.gender}</span>
+        ) : (
+          ""
+        )}
 
+        <br />
+        <br />
 
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          data-testid="password"
+          onChange={handleChange}
+        />
+        <br />
+        {errors !== null && errors.password !== "" ? (
+          <span>{errors.password}</span>
+        ) : (
+          ""
+        )}
+        <br />
+        <br />
+        <label htmlFor="phoneNumber">Phone Number: </label>
+        <input
+          type="text"
+          id="phoneNumber"
+          value={phoneNumber}
+          onChange={handleChange}
+          data-testid="phoneNumber"
+        />
+        <br />
+        {errors !== null && errors.phoneNumber !== "" ? (
+          <span>{errors.phoneNumber}</span>
+        ) : (
+          ""
+        )}
+        <br />
+        <br />
+        <button type="submit" onClick={handleClick} data-testid="submit">
+          Signup
+        </button>
+      </form>
+      {user !== "" ? <p>{user}</p> : ""}
+    </>
+  );
+};
 export default App;
---
+++ src/components/validation.js
@@ -0,0 +1,56 @@
+const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+function isEmailAddress(str) {
+  return str.match(pattern);
+}
+function isAlphaNumeric(str) {
+  var code, i, len;
+
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (
+      !(code > 47 && code < 58) && // numeric (0-9)
+      !(code > 64 && code < 91) && // upper alpha (A-Z)
+      !(code > 96 && code < 123) &&
+      !(str.charAt(i) === " ")
+    ) {
+      // lower alpha (a-z)
+      return false;
+    }
+  }
+  return true;
+}
+
+const signUpFormValidation = (formData) => {
+  const error = {};
+  const fields = ["email", "password", "name", "gender", "phoneNumber"];
+  fields.forEach((field) => {
+    if (!formData[`${field}`]) {
+      error[[field]] = `All fields are mandatory`;
+    }
+  });
+  if (formData.gender && formData.gender === "") {
+    error["gender"] = "Please identify as male, female or others";
+  }
+  if (
+    formData.phoneNumber &&
+    !(formData.phoneNumber.match(/^[0-9]+$/) !== null)
+  ) {
+    error["phoneNumber"] = "Phone Number must contain only numbers";
+  }
+
+  if (formData.name && !isAlphaNumeric(formData.name)) {
+    error["name"] = "Name is not alphanumeric";
+  }
+  if (formData.password && formData.password.length < 6) {
+    error["password"] = "Password must contain atleast 6 letters";
+  }
+  //isEmailAddress(formData.email) === null
+  if (formData.email && !formData.email.includes("@")) {
+    error["email"] = "Email must contain @";
+  }
+  if (Object.keys(error).length === 0) return null;
+  return error;
+};
+
+export { signUpFormValidation };
